4320a0eecd99b07927ee486ec467c305809601b2
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-31 19:00:21 +0530

    Let's start some basic programs

---
+++ Basic_Programs/Basic_Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Some basic programs");
+        }
+    }
+}
